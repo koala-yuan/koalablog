4cd1348927dd56889df3e5ff7b3f120af54ee52b
koala <koala.yuan@linktime.cloud>
2024-06-26 10:09:53 +0800

    test dist dir

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,8 +7,13 @@ import theme from './theme/index.js'
 const __dirname = getDirname(import.meta.url)
 const resolve = (...dirs) => path.resolve(__dirname, ...dirs)
 
+console.log('#### __dirname', __dirname)
+console.log('#### resolve', resolve('public'))
+
 export default defineUserConfig({
   lang: 'zh-CN',
+  base: '/blog/',
+  outDir: './dist',
   locales: {
     '/': { lang: 'zh-CN', title: 'Koala', description: '热爱生活' },
   },
